4f558291702f904325b3475acfddec6ef9d42065
Mihai Barbos <mihai.barbos@gmail.com>
2013-10-22 14:10:09 +0200

    changed font

--- page.net/pages/build-contact.sh
+++ page.net/pages/build-contact.sh
@@ -3,4 +3,8 @@
 # Build the contact page out of the snipets
 
 SNIPDIR="../snippets"
+
+# Change the active menu entry
+TMPFILE="header.$$.tmp"
+
 cat $SNIPDIR/header.html.snip $SNIPDIR/contact.html.snip $SNIPDIR/footer.html.snip > contact.html
