0cbfa3a742574120e35fe9ac0be95f7ae654df15
Andreas Baumann <abaumann@yahoo.com>
2013-10-23 14:54:55 +0200

    fixes and renames in download pages

--- page.net/pages/build-downloads-Wolframe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Build the contact page out of the snipets
-
-SNIPDIR="../snippets"
-cat $SNIPDIR/header.html.snip $SNIPDIR/downloads_Wolframe.html.snip $SNIPDIR/footer.html.snip > downloads_Wolframe.html
--- page.net/pages/buildAll.sh
+++ page.net/pages/buildAll.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 ./build-downloads.sh
-./build-downloads-Wolframe.sh
+./build-downloads-wolframe.sh
 ./build-downloads-wolfclient.sh
 
 mv -f *.html ..
