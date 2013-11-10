feeb5526cb84d38003345bfd16f3b4e64379f93a
Mihai Barbos <mihai.barbos@gmail.com>
2013-11-10 18:08:43 +0100

    changes the slider

--- layout/scripts/custom.js
+++ layout/scripts/custom.js
@@ -50,8 +50,8 @@ jQuery(document).ready(function ($) {
 		auto: true, // Boolean: Animate automatically, true or false
 		speed: 300, // Integer: Speed of the transition, in milliseconds
 		timeout: 4000, // Integer: Time between slide transitions, in milliseconds
-		pager: false, // Boolean: Show pager, true or false
-		nav: true, // Boolean: Show navigation, true or false
+		pager: true, // Boolean: Show pager, true or false
+		nav: false, // Boolean: Show navigation, true or false
 		random: false, // Boolean: Randomize the order of the slides, true or false
 		pause: true, // Boolean: Pause on hover, true or false
 		pauseControls: true, // Boolean: Pause when hovering controls, true or false
