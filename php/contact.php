acdd9b951396ffbcb6b7a9c6feac23ac4065591e
Andreas Baumann <abaumann@yahoo.com>
2013-11-13 16:48:56 +0100

    ..

--- php/contact.php
+++ php/contact.php
@@ -1,20 +1,5 @@
 <?php
 
-//require_once('recaptchalib.php');
-
-/*
-$privatekey = "6LdaNeoSAAAAAJ16_25Mg7WHA9ddSlEitTVhnNHl";
-$resp = recaptcha_check_answer( $privatekey,
-	$_SERVER["REMOTE_ADDR"],
-	$_POST["recaptcha_challenge_field"],
-	$_POST["recaptcha_response_field"] );
-if (!$resp->is_valid) {
-	// What happens when the CAPTCHA was entered incorrectly
-	die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
-	"(reCAPTCHA said: " . $resp->error . ")");
-}
-*/
-
 $author = $_POST["author"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
