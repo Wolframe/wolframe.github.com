6b8d7fb36739d70e9cb783dade2b2c799a9f79fa
Andreas Baumann <abaumann@yahoo.com>
2013-11-22 18:55:48 +0100

    ..

--- build-scripts/build-baddomain.sh
+++ build-scripts/build-baddomain.sh
@@ -6,7 +6,7 @@ SNIPDIR="../snippets"
 
 cat $SNIPDIR/header.html.snip $SNIPDIR/index-baddomain.html.snip $SNIPDIR/footer.html.snip | \
 	sed 's|\(href="\)\([^"]\)|\1http://www.wolframe.ch/\2|g' | \
-	sed 's|\(src="\)|\1http://www:wolframe/ch/|g' | \
+	sed 's|\(src="\)|\1http://www.wolframe.ch/|g' | \
 	sed 's|href="/#|href="#|g' | \
 	sed 's|href="/\(http\)|href="\1|g' | \
 	sed 's|href="/mailto|href="mailto|g' \
