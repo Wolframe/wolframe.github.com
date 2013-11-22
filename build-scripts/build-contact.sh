934265c852a0ef6996ca2462d48c49f435d7b747
Andreas Baumann <abaumann@yahoo.com>
2013-11-22 18:19:29 +0100

    started to eliminate special headers for contact for now

--- build-scripts/build-contact.sh
+++ build-scripts/build-contact.sh
@@ -7,4 +7,6 @@ SNIPDIR="../snippets"
 # Change the active menu entry
 TMPFILE="header.$$.tmp"
 
-cat $SNIPDIR/header_contact.html.snip $SNIPDIR/contact.html.snip $SNIPDIR/footer.html.snip > contact.html
+cat $SNIPDIR/header.html.snip $SNIPDIR/contact.html.snip $SNIPDIR/footer.html.snip | \
+	sed 's|<li class="last-child"|<li class="last-child active"|g' \
+	> contact.html
