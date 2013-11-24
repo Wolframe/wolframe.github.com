ff8886c4e814486eb8a12dec8a2192cf5eaeb9d4
Andreas Baumann <abaumann@yahoo.com>
2013-11-24 10:02:41 +0100

    playing with the footer to make it XHTML compliant started to eliminate the docbook special snippets

--- build-scripts/build-docbook-xmlnippets.sh
+++ build-scripts/build-docbook-xmlnippets.sh
@@ -4,23 +4,30 @@
 
 SNIPDIR="../snippets"
 
-cat $SNIPDIR/header.html.snip $SNIPDIR/doxygen_start.html.snip | \
-	sed 's|\(href="\)\([^"]\)|\1/\2|g' | \
-	sed 's|\(src="\)|\1/|g' | \
-	sed 's|href="/http|href="http|g' | \
-	sed 's|<title>\([^<]*\)</title>|<!--BEGIN PROJECT_NAME--><title>$projectname: $title</title><!--END PROJECT_NAME-->\n	<!--BEGIN !PROJECT_NAME--><title>$title</title><!--END !PROJECT_NAME-->\n\t<meta name="generator" content="Doxygen $doxygenversion"/>|g' | \
-	sed 's|<li><a href="/documentation.html"|<li class="active"><a href="/documentation.html"|g' | \
-	sed -e "/<\/head>/{r $SNIPDIR/doxygen_head.html.snip" -e 'd}' \
-	> header-doxygen-webpage.html
+cat $SNIPDIR/header.html.snip $SNIPDIR/docbook_start.html.snip \
+	> header-webpage.html
 
-cat $SNIPDIR/doxygen_end.html.snip $SNIPDIR/footer.html.snip | \
+touch head-webpage.html
+
+cat > footer-webpage.html <<EOF
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE xsl:stylesheet [
+ <!ENTITY nbsp "&#xa0;">
+ <!ENTITY copy "&#169;">
+]>
+
+<htmlcode>
+EOF
+
+cat $SNIPDIR/docbook_end.html.snip $SNIPDIR/footer.html.snip | \
 	sed 's|\(href="\)\([^"]\)|\1/\2|g' | \
-	sed 's|\(src="\)|\1/|g' | \
-	sed 's|href="//|href="/|g' | \
 	sed 's|href="/#|href="#|g' | \
 	sed 's|src="//|src="/|g' | \
 	sed 's|href="/http|href="http|g' | \
-	sed 's|href="/javascript|href="javascript|g' | \
-	sed 's|src="/http|src="http|g' | \
-	sed -E 's|src="/[$]|src="$|g' \
-	> footer-doxygen-webpage.html
+	sed '/<\/body>/d' | \
+	sed '/<\/html>/d' \
+	>> footer-webpage.html
+
+cat >> footer-webpage.html <<EOF
+</htmlcode>
+EOF
