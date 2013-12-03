a16e07a1c920de6280e0cf3c83e73a61ef9bcadb
Andreas Baumann <abaumann@yahoo.com>
2013-12-03 18:08:36 +0100

    reorganized download pages, removed develop pages

--- build-scripts/buildAll.sh
+++ build-scripts/buildAll.sh
@@ -6,25 +6,8 @@
 ./build-page.sh -f ../snippets/gsearch.html.snip -n > gsearch.html
 ./build-page.sh -f ../snippets/contact.html.snip -a Contact > contact.html
 ./build-page.sh -f ../snippets/copyright.html.snip -n > copyright.html
+./build-page.sh -f ../snippets/downloads.html.snip -n > downloads.html
 
-./build-downloads.sh
-cd downloads
-./build-downloads-wolframe.sh
-./build-downloads-wolfclient.sh
-./build-downloads-wolframe-cjson.sh
-./build-downloads-wolframe-client.sh
-./build-downloads-wolframe-freeimage.sh
-./build-downloads-wolframe-libclient.sh
-./build-downloads-wolframe-libhpdf.sh
-./build-downloads-wolframe-libxml2.sh
-./build-downloads-wolframe-lua.sh
-./build-downloads-wolframe-pam.sh
-./build-downloads-wolframe-postgresql.sh
-./build-downloads-wolframe-python.sh
-./build-downloads-wolframe-sasl.sh
-./build-downloads-wolframe-sqlite3.sh
-./build-downloads-wolframe-textwolf.sh
-cd ..
 ./build-documentation.sh
 #./build-index.sh
 ./build-support.sh
@@ -33,17 +16,9 @@ cd faqs
 ./build-faqs-installation.sh
 ./build-faqs-license.sh
 cd ..
-./build-develop.sh
-cd development
-./build-development_daily_builds.sh
-cd ..
-./build-static-downloads.sh
 ./build-404.sh
-./build-baddomain.sh
 ./build-doxygen-templates.sh
 ./build-docbook-xmlnippets.sh
 
 mv -f *.html ..
-mv -f downloads/*.html ../downloads/.
 mv -f faqs/*.html ../faqs/.
-mv -f development/*.html ../development/.
