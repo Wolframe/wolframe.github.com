ae3d24cff0c75230f94ca453024f820d4b8d11d4
Andreas Baumann <abaumann@yahoo.com>
2013-12-05 17:08:43 +0100

    more cleanup

--- build-scripts/buildAll.sh
+++ build-scripts/buildAll.sh
@@ -6,15 +6,14 @@
 ./build-page.sh -f ../snippets/gsearch.html.snip -n > gsearch.html
 ./build-page.sh -f ../snippets/contact.html.snip -a Contact > contact.html
 ./build-page.sh -f ../snippets/copyright.html.snip -n > copyright.html
-./build-page.sh -f ../snippets/downloads.html.snip -n > downloads.html
-./build-page.sh -f ../snippets/support.html.snip -n > support.html
-./build-page.sh -f ../snippets/faqs.html.snip -n > faqs.html
-./build-page.sh -f ../snippets/faq_license.html.snip -n > faq_license.html
-./build-page.sh -f ../snippets/faq_installation.html.snip -n > faq_installation.html
+./build-page.sh -f ../snippets/documentation.html.snip -a Documentation > documentation.html
+./build-page.sh -f ../snippets/downloads.html.snip -a Downloads > downloads.html
+./build-page.sh -f ../snippets/support.html.snip -a Support > support.html
+./build-page.sh -f ../snippets/faqs.html.snip -a Support > faqs.html
+./build-page.sh -f ../snippets/faq_license.html.snip -a Support > faq_license.html
+./build-page.sh -f ../snippets/faq_installation.html.snip -a Support > faq_installation.html
+./build-page.sh -f ../snippets/404.html.snip -n > 404.html
 
-./build-documentation.sh
-#./build-index.sh
-./build-404.sh
 ./build-doxygen-templates.sh
 ./build-docbook-xmlnippets.sh
 
