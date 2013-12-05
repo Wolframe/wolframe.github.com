3f427d5d50584c7c4d452b60325ab65e3e20e4ff
Andreas Baumann <abaumann@yahoo.com>
2013-12-05 17:00:41 +0100

    some cleanup: removed some scripts, some faqs/* subdirs (which require absolute references), some renames

--- build-scripts/buildAll.sh
+++ build-scripts/buildAll.sh
@@ -7,18 +7,15 @@
 ./build-page.sh -f ../snippets/contact.html.snip -a Contact > contact.html
 ./build-page.sh -f ../snippets/copyright.html.snip -n > copyright.html
 ./build-page.sh -f ../snippets/downloads.html.snip -n > downloads.html
+./build-page.sh -f ../snippets/support.html.snip -n > support.html
+./build-page.sh -f ../snippets/faqs.html.snip -n > faqs.html
+./build-page.sh -f ../snippets/faq_license.html.snip -n > faq_license.html
+./build-page.sh -f ../snippets/faq_installation.html.snip -n > faq_installation.html
 
 ./build-documentation.sh
 #./build-index.sh
-./build-support.sh
-./build-faq.sh
-cd faqs
-./build-faqs-installation.sh
-./build-faqs-license.sh
-cd ..
 ./build-404.sh
 ./build-doxygen-templates.sh
 ./build-docbook-xmlnippets.sh
 
 mv -f *.html ..
-mv -f faqs/*.html ../faqs/.
